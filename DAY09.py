3a67f767ee6be0a30b1632dc1cd7ab66f4b2c469
Bhagavath-G <bhagavathg1382008@gmail>
2026-08-10 09:05:50 +0530

    Initial commit

---
+++ DAY09.py
@@ -0,0 +1,14 @@
+''' PROJECT 9'''
+
+a = int(input("enter first number:"))
+b = int(input("enter second number:"))
+
+print("BEFORE SWAPPING")
+print("a=",a)
+print("b=",b)
+
+a,b = b,a
+print("AFTER SWAPPING")
+
+print("a=",a)
+print("b=",b)
