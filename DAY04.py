0165ffbd9ba1b08ae9b43ecf40e72b4b41017a39
Bhagavath-G <bhagavathg1382008@gmail>
2026-08-10 08:59:53 +0530

    Inital commit

---
+++ DAY04.py
@@ -0,0 +1,11 @@
+''' PROJECT 4 '''
+
+num1=int(input("enter first number"))
+num2=int(input("enter second number"))
+print("ALL ANSWERS")
+print("ADDITION", num1+num2)
+print("SUBRACTION", num1-num2)
+print("MULTIPLICATION", num1*num2)
+print("DIVISION", num1/num2)
+print("MODULO", num1%num2)
+print("FLOOR_DIVISION", num1//num2)
