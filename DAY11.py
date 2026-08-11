9a278c7f2fc9351a0b388bd654a52fbf35c4d07f
Bhagavath-G <bhagavathg1382008@gmail>
2026-08-11 08:58:03 +0530

    Initial commit

---
+++ DAY11.py
@@ -0,0 +1,12 @@
+'''PROJECTS 11'''
+
+task = []
+task1 = input("Enter your task 1 :")
+task.append(task1)
+task2 = input("Enter your task 2 :")
+task.append(task2)
+task3 = input("Enter your task 3 :")
+task.append(task3)
+task4 = input("Enter your task 4 :")
+task.append(task4)
+print("YOUR TO DO LIST IS :", task)
