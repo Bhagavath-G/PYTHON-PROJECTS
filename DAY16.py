6c968895008370ac065fe58ee695e0fcc96bf0fd
Bhagavath-G <bhagavathg1382008@gmail>
2026-08-16 07:53:59 +0530

    Initial commiy

---
+++ DAY16.py
@@ -0,0 +1,11 @@
+
+'''PROJECT 16'''
+
+username="bhagavathkumar"
+password="1234"
+u = input("Enter your username:")
+p = input("Enter your password:")
+if u == username and p == password:
+    print("LOGIN SUCCESFULLY!!!")
+else:
+    print("LOGIN FAILED!!!")
